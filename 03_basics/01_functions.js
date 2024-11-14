91424f49231d09eb3cf6d2baea887556666f6a5f
Shresth Dwivedi <129939594+ShresthDw@users.noreply.github.com>
2024-11-14 19:28:58 +0000

    functions in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,7 +38,11 @@ const result  = addTwoNumbers(3,4)
 
 console.log( "result : " , result);   // type --> undefined
 
-function loginUserMessage(username){
+function loginUserMessage(username = "sam"){        // will not go in if statement.
+    if (username === undefined){    // (username === undefined) ---> !username
+        console.log(" please enter a username");
+        return   // output --> undefined 
+    }
     return ` ${username} just loggedin`
 }
 
@@ -46,5 +50,50 @@ loginUserMessage("shresth") // will print nothing yet
 
 console.log(loginUserMessage("shresth"));
 
+console.log(loginUserMessage(""));  //output ->  just loggedin
 
+console.log(loginUserMessage());  // output --> undefined just loggedin 
+
+
+function calculateCartPrice ( val1, val2, ...num){     //rest operator/spread 
+    return num1
+}
+
+console.log(calculateCartPrice(200, 300, 400, 500));
+
+const user = {
+    username : "shresth",
+    price : 199
+}
+
+
+function handleObject(anyobject){
+    console.log(` Username is ${anyobject.username} and price is ${anyobject.price} `);
+    
+}
+
+
+// handleObject(user)
+
+handleObject({
+    username: "sam" ,
+    price: 399
+})
+
+
+
+// passing Arrays
+
+
+const myNewArary =[ 200, 300, 400]
+
+
+function returnSecondValue(getArray){
+    return getArray[1]  //--> output --> 400
+}
+
+// console.log(returnSecondValue(myNewArary));
+
+
+console.log(returnSecondValue([200, 300, 400]));    // passing array directly
 
